8b34d2a176417f49ac1bd128953178f936b0444a
OakBranches.M <50147318+OakBranches@users.noreply.github.com>
2022-04-26 22:42:24 -0300

    Convert to rgb

--- generate.py
+++ generate.py
@@ -23,7 +23,7 @@ def insertDiego(filepath):
 
     angle = randint(0,360)
     d = Image.open(d_filepath)
-    factor = 1/randint(1,10)
+    factor = 1/randint(2,10)
     diego = d.resize((round(back.size[0]*factor), round(back.size[1]*factor))).convert('RGBA')
     position = (randint(0,back.size[0]-diego.size[0]), randint(0,back.size[1]-diego.size[1]))
     diego = diego.rotate(angle, expand=True)
@@ -44,15 +44,15 @@ for filename in os.listdir(a_directory):
         filepath = os.path.join(a_directory, filename)
         i = randrange(1,100)
         j = randrange(1,100)
-        filename = filename.split(".")[0]+'.png'
+        filename = filename.split(".")[0]+'.jpg'
         
         prefix = "train" if j < 70 else "valid" if j < 80 else "test"
         if i > 50:
             img = insertDiego(filepath)
-            img.save(f'./Output/{prefix}/Yes/{filename}')
+            img.convert('RGB').save(f'./Output/{prefix}/Yes/{filename}')
         else:
             img = Image.open(filepath).resize((200,200))
-            img.save(f'./Output/{prefix}/No/{filename}')
+            img.convert('RGB').save(f'./Output/{prefix}/No/{filename}')
     except:
         pass
 
